843d5f6ca2c1dee72889e9a792934edf514877b0
darkstalker <slayerbeast@gmail.com>
2015-07-14 13:49:31 -0300

    implemented builtin "set"

--- src/builtins.rs
+++ src/builtins.rs
@@ -10,6 +10,22 @@ pub fn load(env: &mut GlobalScope)
         }
     });
 
+    env.set_builtin("set", true, |args, env| {
+        match *args {
+            Some(ref c1) => match c1.cdr {
+                Some (ref c2) => match c1.car {
+                    Value::Symbol(ref name) => {
+                        env.set(name, c2.car.clone());
+                        Ok(c2.car.clone())
+                    }
+                    ref other => Err(RuntimeError::InvalidArgType("Symbol", other.type_name())),
+                },
+                None => Err(RuntimeError::InvalidArgNum(2)),
+            },
+            None => Err(RuntimeError::InvalidArgNum(2)),
+        }
+    });
+
     env.set_builtin("+", true, |mut args, _| {
         let mut acc = 0.0;
         while let Some(ref cons) = *args
